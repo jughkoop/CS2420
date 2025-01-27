1e9fde08ded817aaafe86d0b751b38eb64e84052
Daniel Lee <57880066+jughkoop@users.noreply.github.com>
2025-01-27 15:38:58 -0700

    Add files via upload

--- assign2/SimplePriorityQueue.java
+++ assign2/SimplePriorityQueue.java
@@ -4,18 +4,40 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.NoSuchElementException;
 
+/**
+ * This class is a simple priority queue.
+ * Users only have access to the maximum element in the queue, which
+ * is also the last element in the queue. The queue is sorted as items
+ * are inserted into it.
+ * 
+ * @author Daniel Lee and Mi Zeng
+ * @verison 1-27-2025
+ * @param <E> - the type of elements contained in this priority queue
+ */
 public class SimplePriorityQueue<E> implements PriorityQueue<E>{
 	private E[] backingArray;
 	private int size;
     private Comparator<? super E> cmp;
     
+    /**
+     * Constructs an empty SimplePriorityQueue
+     * backed by an array of 16 elements,
+     * with its Comparator being the natural order Comparator.
+     */
     @SuppressWarnings("unchecked")
 	public SimplePriorityQueue() {
     	backingArray = (E[]) new Object[16];
     	size = 0;
-    	cmp = null;
+    	cmp = (Comparator<? super E>)Comparator.naturalOrder();
     }
     
+    /**
+     * Constructs an empty SimplePriorityQueue
+     * backed by an array of 16 elements,
+     * with its Comparator being the custom Comparator given.
+     * 
+     * @param cmp - the given custom Comparator
+     */
     @SuppressWarnings("unchecked")
 	public SimplePriorityQueue(Comparator<? super E> cmp) {
     	backingArray = (E[]) new Object[16];
@@ -23,18 +45,34 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E>{
     	this.cmp = cmp;
     }
     
+    /**
+	 * Removes all of the elements from this priority queue. The queue will be
+	 * empty when this call returns.
+	 */
 	@Override
 	public void clear() {
 		size = 0;
 	}
 
+	/**
+	 * Indicates whether this priority queue contains the specified element.
+	 * 
+	 * @param item - the element to be checked for containment in this priority queue
+	 */
 	@Override
 	public boolean contains(E item) {
-		if(backingArray[binarySearch(item, cmp)] == item)
-			return true;
+		if(item.equals(backingArray[binarySearch(item, cmp)]))
+			return true; 
 		return false;
 	}
 
+	/**
+	 * Indicates whether this priority contains all of the specified elements.
+	 * 
+	 * @param coll - the collection of elements to be checked for containment in this priority queue
+	 * @return true if this priority queue contains every element in the specified collection;
+	 *         otherwise, returns false
+	 */
 	@Override
 	public boolean containsAll(Collection<? extends E> coll) {
 		for(E item : coll)
@@ -43,6 +81,12 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E>{
 		return true;
 	}
 
+	/**
+	 * Retrieves and removes the maximum element in this priority queue.
+	 * 
+	 * @return the maximum element
+	 * @throws NoSuchElementException if the priority queue is empty
+	 */
 	@Override
 	public E deleteMax() throws NoSuchElementException {
 		if(isEmpty())
@@ -53,6 +97,13 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E>{
 		return max;
 	}
 
+	/**
+	 * Retrieves, but does not remove, the maximum element in this priority
+	 * queue.
+	 * 
+	 * @return the maximum element
+	 * @throws NoSuchElementException if the priority queue is empty
+	 */
 	@Override
 	public E findMax() throws NoSuchElementException {
 		if(isEmpty())
@@ -60,6 +111,11 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E>{
 		return backingArray[size-1];
 	}
 
+	/**
+	 * Inserts the specified element into this priority queue.
+	 * 
+	 * @param item - the element to insert
+	 */
 	@Override
 	public void insert(E item) {
 		if(size == backingArray.length) 
@@ -70,34 +126,59 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E>{
 		size++;
 	}
 
+	/**
+	 * Inserts the specified elements into this priority queue.
+	 * 
+	 * @param coll - the collection of elements to insert
+	 */
 	@Override
 	public void insertAll(Collection<? extends E> coll) {
 		for(E item : coll)
 			insert(item);
 	}
 
+	/**
+	 * Indicates whether priority queue contains any elements.
+	 * 
+	 * @return true if this priority queue is empty; false otherwise
+	 */
 	@Override
 	public boolean isEmpty() {
-		if(size == 0) {
+		if(size == 0)
 			return true;
-		}
 		return false;
 	}
 
+	/**
+	 * Determines the number of elements in this priority queue.
+	 * 
+	 * @return the number of elements in this priority queue
+	 */
 	@Override
 	public int size() {
 		return size;
 	}
 	
+	/**
+	 * Performs a binary search to find the given target within
+	 * the backing array, using the given Comparator to compare the 
+	 * target to items in the array, returning the index the target is in.
+	 * 
+	 * If the binary search cannot find the target, it instead returns
+	 * the index where the target would be in if present in the array.
+	 * 
+	 * @param target - the item to search for
+	 * @param cmp - the Comparator to use
+	 * @return the index of the target or the index where the target would be
+	 */
 	@SuppressWarnings("unchecked")
 	private int binarySearch(E target, Comparator<? super E> cmp) {
 		int left = 0;
 		int right = size;
 		int center;
-
 		while(left < right){
 			center = left + (right - left) / 2;
-			if(cmp.compare(backingArray[center], target) == 0)
+			if(backingArray[center].equals(target))
 				return center;
 
 			if(cmp.compare(backingArray[center], target) < 0)
@@ -105,33 +186,18 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E>{
 			else
 				right = center;
 		}
-		
-		if(cmp.equals(null)) {
-			while(left < right){
-				center = left + (right - left) / 2;
-				if(((Comparable<? super E>)backingArray[center]).compareTo(target) == 0)
-					return center;
-
-				if(((Comparable<? super E>)backingArray[center]).compareTo(target) < 0)
-					left = center + 1;
-				else
-					right = center;
-			}
-		}
 		return left;
 	}
 	
 	/**
-	 * Creates a new array with twice the length as the backing array.
-	 * Copies all elements from the backing array to the new array.
-	 * Sets the backing array reference to the new array.
+	 * Doubles the size of the backing array.
 	 */
 	@SuppressWarnings("unchecked")
 	private void doubleBackingArray() {
-		E[] largerArray = (E[])new Object[backingArray.length * 2];
+		E[] newArray = (E[])new Object[backingArray.length * 2];
 		for(int i = 0; i < backingArray.length; i++) 
-			largerArray[i] = backingArray[i];			
-		backingArray = largerArray;
+			newArray[i] = backingArray[i];			
+		backingArray = newArray;
 	}
 
 }
--- assign2/SimplePriorityQueueTest.java
+++ assign2/SimplePriorityQueueTest.java
@@ -2,13 +2,162 @@ package assign03;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Random;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * For testing the SimplePriorityQueue class.
+ * 
+ * @author Daniel Lee and Mi Zeng
+ * @version 1-27-2025
+ */
 class SimplePriorityQueueTest {
 
+	private SimplePriorityQueue<Integer> integerQueue;
+	private SimplePriorityQueue<Double> doubleQueue;
+	private SimplePriorityQueue<Integer> randomQueue;
+	private SimplePriorityQueue<String> stringQueue;
+	private int[] arr = new int[100];
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		integerQueue = new SimplePriorityQueue<Integer>();
+		integerQueue.insert(15);
+		integerQueue.insert(25);
+		integerQueue.insert(35);
+		integerQueue.insert(45);
+		integerQueue.insert(55);
+		
+		doubleQueue = new SimplePriorityQueue<Double>();
+		doubleQueue.insert(1.8);
+		doubleQueue.insert(3.4);
+		doubleQueue.insert(6.8);
+		
+		randomQueue= new SimplePriorityQueue<Integer>();
+		Random randomNumber = new Random();
+		for(int i = 0;  i < arr.length; i++) {
+			arr[i] = randomNumber.nextInt(100);
+			randomQueue.insert(arr[i]);
+		}
+		Arrays.sort(arr);
+		
+		stringQueue = new SimplePriorityQueue<String>((String str1, String str2) -> str2.compareTo(str1));
+		stringQueue.insert("apple");
+		stringQueue.insert("banana");
+		stringQueue.insert("carrot");
+		stringQueue.insert("date");
+		
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testClear(){
+		integerQueue.clear();
+		assertEquals(0, integerQueue.size(), "The elements of the array size should be 0");
+	}
+	
+	@Test
+	public void testFindMaxValue(){
+		assertEquals(55, integerQueue.findMax(),"The maxiumum value shoule be 55");
+		assertEquals(6.8, doubleQueue.findMax(), "The maxiumum value should be 6.8");
+		assertEquals(arr[arr.length-1], randomQueue.findMax(),"The maxiumum value should be the last index" );
+		assertEquals("apple", stringQueue.findMax(), "The maxiumum value should be according to the custom comparator");
+	}
+	
+	@Test
+	public void testFindMaxNoSuchElementException() {
+		doubleQueue.clear();
+		assertThrows(NoSuchElementException.class, () -> { doubleQueue.findMax(); });
+	}
+	
+	@Test
+	public void testContainsElements(){
+		assertTrue(integerQueue.contains(55), "The value should be in the array");
+		assertFalse(integerQueue.contains(100),"The value should not be in the array ");
+		assertTrue(doubleQueue.contains(6.8), "The value should be in the array");
+		assertFalse(doubleQueue.contains(8.8),"The value should not be in the array");
+		assertTrue(stringQueue.contains("apple"),"The value should be in the array");
+		assertFalse(stringQueue.contains("orange"),"The value should not be in the array");
+	}
+	
+	@Test
+	public void testContainsAll() {
+		ArrayList<Integer> intList = new ArrayList<Integer>();
+		intList.add(15);
+		intList.add(25);
+		intList.add(35);
+		intList.add(45);
+		intList.add(55);
+		assertTrue(integerQueue.containsAll(intList),"The array should contain all the elements");
+		intList.add(65);
+		assertFalse(integerQueue.containsAll(intList), "The array should not contain all the elements");
+	}
+	
+	@Test
+	public void testDeleteMax() {
+		assertEquals(55, integerQueue.deleteMax(), "The maximum value should be 55");
+		assertEquals(6.8, doubleQueue.deleteMax(), "The maxiumum value should be 6.8");
+		assertEquals(arr[arr.length-1], randomQueue.deleteMax(),"The maxiumum value should be the last index" );
+	}
+	
+	@Test
+	public void testDeleteMaxSize() {
+		integerQueue.deleteMax();
+		doubleQueue.deleteMax();
+		randomQueue.deleteMax();
+		assertEquals(4, integerQueue.size(),"The array size shoule be 4");
+		assertEquals(2, doubleQueue.size(), "The array size should be 2");
+		assertEquals(99, randomQueue.size(),"The array size should be 99");
+	}
+	
+	@Test
+	public void testDeleteMaxNoSuchElementException() {
+		doubleQueue.clear();
+		assertThrows(NoSuchElementException.class, () -> { doubleQueue.deleteMax(); });
+	}
+	
+	@Test
+	public void testInsertSize() {
+		integerQueue.insert(65);
+		doubleQueue.insert(9.7);
+		randomQueue.insert(257);
+		assertEquals(6, integerQueue.size(), "The array size should be 6");
+		assertEquals(4, doubleQueue.size(),"The array size should be 4");
+		assertEquals(101, randomQueue.size(),"The array size should be 101");
+	}
+	
+	@Test
+	public void testInsertAllSize() {
+		ArrayList<Integer> intList = new ArrayList<Integer>();
+		intList.add(15);
+		intList.add(65);
+		intList.add(35);
+		intList.add(75);
+		intList.add(55);
+		integerQueue.insertAll(intList);
+		assertEquals(10, integerQueue.size(),"The array size should be 10");
+	}
+	
+	@Test
+	public void testIsEmpty() {
+		assertFalse(integerQueue.isEmpty(), "The array should not be empty");
+		integerQueue.clear();
+		assertTrue(integerQueue.isEmpty(), "The array should be empty");
+	}
+	
+	@Test
+	public void testComparatorFindMax() {
+		ArrayList<String> strList = new ArrayList<String>();
+		strList.add("date");
+		strList.add("carrot");
+		strList.add("banana");
+		strList.add("apple");
+		
+		assertEquals("apple", stringQueue.findMax(), "The maximum should be "
+				+ "reverse lexocographical order according to the comparator");
 	}
 
 }
