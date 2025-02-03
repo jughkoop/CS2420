448760b5a9bb2d9cbbbe7fa59a8561acec32317d
Daniel Lee <57880066+jughkoop@users.noreply.github.com>
2025-02-03 12:08:46 -0700

    Add files via upload

---
+++ LargestNumberSolverTest.java
@@ -0,0 +1,103 @@
+package assign04;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LargestNumberSolverTest {
+
+	@Test
+	public void testInsertionSortSmallSize() {
+		
+	}
+	
+	@Test
+	public void testInsertionSortMediumSize() {
+		
+	}
+	
+	@Test
+	public void testInsertionSortLargeSize() {
+		
+	}
+	
+	@Test
+	public void testInserstionSortDuplicates() {
+		
+	}
+	
+	@Test
+	public void testFindLargestNumberFromSmallNumbers() {
+		
+	}
+	
+	@Test
+	public void testFindLargestNumberFromLargeNumbers() {
+		
+	}
+	
+	@Test
+	public void testFindLargestNumberFromVeryLargeNumbers() {
+		
+	}
+	
+	@Test
+	public void testFindLargestIntSmallInts() {
+		
+	}
+	
+	@Test
+	public void testFindLargestIntLargeInts() {
+		
+	}
+	
+	@Test
+	public void testFindLargestIntOutOfRangeException() {
+		
+	}
+	
+	@Test
+	public void testFindLargestLongSmallLongs() {
+		
+	}
+	
+	@Test
+	public void testFindLargestLongLargeLongs() {
+		
+	}
+	
+	@Test
+	public void testFindLargestLongVeryLargeLongs() {
+		
+	}
+	
+	@Test
+	public void testFindLargestLongOutOfRangeException() {
+		
+	}
+	
+	@Test
+	public void testSumSmall() {
+		
+	}
+	
+	@Test
+	public void testSumLarge() {
+		
+	}
+	
+	@Test
+	public void testSumVeryLarge() {
+		
+	}
+	
+	@Test
+	public void readFile() {
+		
+	}
+	
+	
+	
+	
+
+}
