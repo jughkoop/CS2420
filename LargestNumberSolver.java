f29e0258ec4c0c498e8290f648f2a3feadf90b76
Daniel Lee <57880066+jughkoop@users.noreply.github.com>
2025-02-03 12:03:00 -0700

    Add files via upload

--- LargestNumberSolver.java
+++ LargestNumberSolver.java
@@ -33,39 +33,39 @@ public class LargestNumberSolver {
 	
 	public static int findLargestInt(Integer[] arr) throws OutOfRangeException {
 		// copy arr to new array arrayCopy
-				Integer[] arrayCopy = new Integer[arr.length];
-				for(int i = 0; i < arr.length; i++)
-					arrayCopy[i] = arr[i];
-				// sorts the array copy
-				insertionSort(arrayCopy, (i, j) -> Integer.parseInt(intConcatenate(i, j)) - Integer.parseInt(intConcatenate(j, i)));
-				int largestInt = arrayCopy[0];
-				// concatenates each value of the sorted array into a BigInteger
-				for(int i = 1; i < arr.length; i++) {
-					if(Integer.parseInt((intConcatenate(largestInt, arrayCopy[i]))) > Integer.MAX_VALUE)
-						throw new OutOfRangeException("int");
-					largestInt = Integer.parseInt((intConcatenate(largestInt, arrayCopy[i])));
-				}
+		Integer[] arrayCopy = new Integer[arr.length];
+		for(int i = 0; i < arr.length; i++)
+			arrayCopy[i] = arr[i];
+		// sorts the array copy
+		insertionSort(arrayCopy, (i, j) -> Integer.parseInt(intConcatenate(i, j)) - Integer.parseInt(intConcatenate(j, i)));
+		int largestInt = arrayCopy[0];
+		// concatenates each value of the sorted array into an int
+		for(int i = 1; i < arr.length; i++) {
+			if(Integer.parseInt((intConcatenate(largestInt, arrayCopy[i]))) > Integer.MAX_VALUE)
+					throw new OutOfRangeException("int");
+			largestInt = Integer.parseInt((intConcatenate(largestInt, arrayCopy[i])));
+		}
 				
-				return largestInt;
+		return largestInt;
 		
 	}
 	
 	public static long findLargestLong(Integer[] arr) throws OutOfRangeException {
 		// copy arr to new array arrayCopy
-		Long[] arrayCopy = new Long[arr.length];
+		Integer[] arrayCopy = new Integer[arr.length];
 		for(int i = 0; i < arr.length; i++)
-			arrayCopy[i] = (long)arr[i];
+			arrayCopy[i] = arr[i];
 		// sorts the array copy
 		insertionSort(arrayCopy, (i, j) -> Integer.parseInt(intConcatenate(i, j)) - Integer.parseInt(intConcatenate(j, i)));
 		long largestLong = arrayCopy[0];
-		// concatenates each value of the sorted array into a BigInteger
+		// concatenates each value of the sorted array into a long
 		for(int i = 1; i < arr.length; i++) {
 			if(Integer.parseInt((intConcatenate(largestLong, arrayCopy[i]))) > Long.MAX_VALUE)
-				throw new OutOfRangeException("int");
-			largestInt = Integer.parseInt((intConcatenate(largestInt, arrayCopy[i])));
+				throw new OutOfRangeException("long");
+			largestLong = Integer.parseInt((intConcatenate(largestLong, arrayCopy[i])));
 		}
 		
-		return largestInt;
+		return largestLong;
 	}
 	
 	public static BigInteger sum(List<Integer[]> list) {
