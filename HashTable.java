08a37ba47ece457061fb94fe2122e34ac3bef027
Daniel Lee <57880066+jughkoop@users.noreply.github.com>
2025-03-31 11:14:02 -0600

    Add files via upload

--- HashTable.java
+++ HashTable.java
@@ -2,40 +2,65 @@ package assign09;
 
 import java.util.LinkedList;
 import java.util.List;
-
+/**
+ * This class implements a HashTable consisting of unique key-nonunique value pairs,
+ * using the separate chaining strategy strategy to handle collisions. All basic operations
+ * take O(1) time.
+ * 
+ * @author Daniel Lee & Mi Zeng
+ * @version 3-31-2025
+ * @param <K> - placeholder for the key type
+ * @param <V> - placeholder for the value type
+ */
 public class HashTable<K, V> implements Map<K, V> {
 	private Object[] table;
 	private int size;
 	private double maxLoadFactor;
 	
+	/**
+	 * Creates a new HashTable with a default max load factor of 10.
+	 */
 	public HashTable() {
 		table = new Object[10];
 		for(int i = 0; i < table.length; i++)
+			// initializes each index of the table to a linked list chain to hold entries
 			table[i] = new LinkedList<MapEntry<K, V>>();
 		size = 0;
 		maxLoadFactor = 10;
 	}
 	
+	/**
+	 * Creates a new HashTable with a given max load factor.
+	 * 
+	 * @param maxLoadFactor - the max load factor before the table must
+	 * 						  be remapped
+	 * @throws IllegalArgumentException if the given max load factor is less than 1
+	 */
 	public HashTable(double maxLoadFactor) {
 		if(((Double)maxLoadFactor).compareTo(1.0) < 0)
 			throw new IllegalArgumentException();
 		
 		table = new Object[10];
 		for(int i = 0; i < table.length; i++)
+			// initializes each index of the table to a linked list chain to hold entries
 			table[i] = new LinkedList<MapEntry<K, V>>();
 		size = 0;
 		this.maxLoadFactor = maxLoadFactor;
 	}
 	
+	/**
+	 * Clears the hash table.
+	 */
 	@Override
 	public void clear() {
 		for(int i = 0; i < table.length; i++)
 			getChain(i).clear();
 	}
 
+	
 	@Override
 	public boolean containsKey(K key) {
-		LinkedList<MapEntry<K, V>> chain = getChain(key.hashCode() % table.length);
+		LinkedList<MapEntry<K, V>> chain = getChain(Math.abs(key.hashCode() % table.length));
 		for(MapEntry<K, V> entry : chain)
 			if(entry.getKey().equals(key))
 				return true;
@@ -67,7 +92,7 @@ public class HashTable<K, V> implements Map<K, V> {
 	@Override
 	public V get(K key) {
 		if(containsKey(key)) {
-			LinkedList<MapEntry<K, V>> chain = getChain(key.hashCode() % table.length);
+			LinkedList<MapEntry<K, V>> chain = getChain(Math.abs(key.hashCode() % table.length));
 			for(MapEntry<K, V> entry : chain)
 				if(entry.getKey().equals(key))
 					return entry.getValue();
@@ -82,7 +107,7 @@ public class HashTable<K, V> implements Map<K, V> {
 
 	@Override
 	public V put(K key, V value) {
-		LinkedList<MapEntry<K, V>> chain = getChain(key.hashCode() % table.length);
+		LinkedList<MapEntry<K, V>> chain = getChain(Math.abs(key.hashCode() % table.length));
 		if(containsKey(key)) {
 			for(MapEntry<K, V> entry : chain)
 				if(entry.getKey().equals(key)) {
@@ -101,7 +126,7 @@ public class HashTable<K, V> implements Map<K, V> {
 
 	@Override
 	public V remove(K key) {
-		LinkedList<MapEntry<K, V>> chain = getChain(key.hashCode() % table.length);
+		LinkedList<MapEntry<K, V>> chain = getChain(Math.abs(key.hashCode() % table.length));
 		if(containsKey(key)) {
 			for(MapEntry<K, V> entry : chain)
 				if(entry.getKey().equals(key)) {
@@ -133,7 +158,7 @@ public class HashTable<K, V> implements Map<K, V> {
 		for(int i = 0; i < table.length; i++) {
 			LinkedList<MapEntry<K, V>> chain = getChain(i);
 			for(MapEntry<K, V> entry : chain)
-				((LinkedList<MapEntry<K, V>>)newTable[entry.getKey().hashCode() % newTable.length]).add(entry);
+				((LinkedList<MapEntry<K, V>>)newTable[Math.abs(entry.getKey().hashCode() % newTable.length)]).add(entry);
 		}
 		table = newTable;
 	}
